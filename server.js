e6ae03543617fd23c7800df3c158a21af4fd0e06
AnthoLC77 <mishynaro@hotmail.fr>
2024-01-08 13:49:06 +0100

    Mise en place du serveur et des configs

--- server.js
+++ server.js
@@ -1,18 +1,48 @@
 // Chargement des variables d'environnement
-const express = require("express");
-const bodyParser = require("body-parser");
+require("dotenv").config();
 
 // Import des modules nécessaires
-// Import des routes pour l'authentification
-// Import de la configuration de la base de données
-// Initialisation de l'application Express
-// Middleware pour traiter les requêtes JSON
-// Middleware pour parser les corps de requêtes
-// Utilisation  des routes pour l'authentification
-// Configuration des options cors
-// Middleware pour gerer les cors
-// Définition du port de demarrage du serveur
-// Fonction pour demarrer le serveur
-// Appel de la fonction pour demarrer le serveur
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
-const app = express;
+// Import des routes pour l'authentification
+
+// Import de la configuration de la base de données
+
+// Initialisation de l'application Express
+const app = express();
+
+// Middleware pour traiter les requêtes JSON
+app.use(express.json());
+
+// Middleware pour parser les corps de requêtes
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Utilisation  des routes pour l'authentification
+
+// Configuration des options cors
+const corsOptions = {
+  credentials: true,
+  optionsSuccessStatus: 200,
+  methods: "GET, POST, PUT, DELETE, PATCH, HEAD",
+  preflightContinue: false,
+};
+
+// Définition du port de demarrage du serveur
+const PORT = process.env.PORT || 5200;
+
+// Fonction pour demarrer le serveur
+const start = async () => {
+  try {
+    // Connecion a la base de données
+    // Demarrage du serveur le port spécifié
+    app.listen(PORT, () => console.log(`Le serveur à démarrer sur le ${PORT}`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// Appel de la fonction pour demarrer le serveur
+start();
