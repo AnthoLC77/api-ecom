79e0500e3bb13dc99f125cdbf57021bfb0b0838f
AnthoLC77 <mishynaro@hotmail.fr>
2024-01-08 11:53:11 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,18 @@
+// Chargement des variables d'environnement
+const express = require("express");
+const bodyParser = require("body-parser");
+
+// Import des modules nécessaires
+// Import des routes pour l'authentification
+// Import de la configuration de la base de données
+// Initialisation de l'application Express
+// Middleware pour traiter les requêtes JSON
+// Middleware pour parser les corps de requêtes
+// Utilisation  des routes pour l'authentification
+// Configuration des options cors
+// Middleware pour gerer les cors
+// Définition du port de demarrage du serveur
+// Fonction pour demarrer le serveur
+// Appel de la fonction pour demarrer le serveur
+
+const app = express;
