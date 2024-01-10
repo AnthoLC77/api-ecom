bddcca5f3add9d3f972fafd07d0a0faedac4e20c
AnthoLC77 <mishynaro@hotmail.fr>
2024-01-10 11:44:28 +0100

    authenticate token

---
+++ middleware/authenticate.js
@@ -0,0 +1,39 @@
+// Import du modèle utilisateur
+const autheModel = require('../models/auth.model');
+// Import du JWT
+const jwt = require('jsonwebtoken');
+
+// Fonction pour la gestion de rôle
+
+module.exports.authenticate = async (req, res, next) => {
+	try {
+		// Definition de la variable pour l'autorisation
+		const authHeader = req.header('Authorization');
+		// Condition qui vérifie la variable et qui ajoute un bearer comme exeption
+		if (!authHeader || !authHeader.startsWith('Bearer ')) {
+			return res.status(401).json({
+				message:
+					"Vous devez être connecté en tant qu'administrateur pour acceder à cette page",
+			});
+		}
+		// Extraction du token sans le prefixe 'Bearer'
+		const token = authHeader.split(' ')[1];
+
+		// Ajout de la variable pour decoder le token
+		const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+		// Déclaration d'une variable qui va recuperer l'id de l'user et lui assigner un token
+		const user = await autheModel.findById(decoded.user.id);
+
+		// Si il n'y a pas d'utilisateur renvoie un message
+		if (!user) {
+			return res.status(400).json({ message: 'Utilisateur non trouvé' });
+		}
+		// Le else n'est pas souhaitable car en locale tout vas fonctionner mais au déploiement il y aura une erreur "req.user is not a function"
+		req.user = user;
+		next();
+	} catch (err) {
+		console.error("Erreur lors de l'authentification : ", err.message);
+		res.status(500).json({ message: "Erreur lors de l'authentification" });
+	}
+};
