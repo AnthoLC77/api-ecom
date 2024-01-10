048c57b50a0d71fb202f73f82b83be7764717956
AnthoLC77 <mishynaro@hotmail.fr>
2024-01-10 12:01:04 +0100

    Creation de la route (dashboard) protégé

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,8 +1,25 @@
-const router = require("express").Router();
-const authController = require("../controllers/auth.controller");
+const router = require('express').Router();
+const authController = require('../controllers/auth.controller');
+const authMiddleware = require('../middleware/authenticate');
 
 // Route pour l'inscription
-router.post("/register", authController.register);
-router.post("/login", authController.login);
+router.post('/register', authController.register);
+// Route pour la connexion
+router.post('/login', authController.login);
+// Route protégée
+router.get('/dashboard', authMiddleware.authenticate, (req, res) => {
+	// Verifier si l'utilisateur est un admin
+	if (req.user.role === admin) {
+		// Definition de req.isAdmin sera égal à true pour les admins
+		req.isAdmin = true;
+		// Envoyer une réponse avec success
+		return res.status(200).json({ message: 'Bienvenue admin !' });
+	} else {
+		// Envoyer une réponse pour les utilisateurs non admin
+		res.status(403).json({
+			message: 'Action non autorisée, seus les admin peuvent acceder à cette page',
+		});
+	}
+});
 
 module.exports = router;
